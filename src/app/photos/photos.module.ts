2462207373a1eb4565af2a95a4f74584ad89b1b5
Victor Duarte <victor.mendonca@unigranrio.br>
2020-10-17 19:31:49 -0300

    refactoring modules

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,25 +1,14 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {HttpClientModule} from "@angular/common/http";
 
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import {PhotoComponent} from './photo/photo.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import {FilterByDescriptionPipe} from "./photo-list/filter-by-description.pipe";
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import {PhotoListModule} from "./photo-list/photo-list.module";
+import {PhotoFormModule} from "./photo-form/photo-form.module";
+import { PhotoModule } from './photo/photo.module';
 
 @NgModule({
-  declarations: [
-    PhotoComponent,
-    PhotoListComponent,
-    PhotoFormComponent,
-    PhotosComponent,
-    FilterByDescriptionPipe,
-    LoadButtonComponent],
   imports: [
-    CommonModule,
-    HttpClientModule,
+    PhotoModule,
+    PhotoListModule,
+    PhotoFormModule,
   ],
 })
 export class PhotosModule {
