8663d4ce8f0eaedf61d8b66dd0f4fbbb52320790
Victor Duarte <victor.mendonca@unigranrio.br>
2020-10-18 10:21:32 -0300

    created custom directive

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -7,6 +7,7 @@ import {FilterByDescriptionPipe} from "./filter-by-description.pipe";
 import {PhotoModule} from "../photo/photo.module";
 import {CardModule} from "../../shared/components/card/card.module";
 import { SearchComponent } from './search/search.component';
+import {DarkenOnHoverModule} from "../../shared/directives/darken-on-hover/darken-on-hover.module";
 
 
 
@@ -21,7 +22,8 @@ import { SearchComponent } from './search/search.component';
   imports: [
     CommonModule,
     PhotoModule,
-    CardModule
+    CardModule,
+    DarkenOnHoverModule
   ]
 })
 export class PhotoListModule { }
---
+++ src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
@@ -0,0 +1,21 @@
+import {Directive, ElementRef, HostListener, Input, Renderer2} from "@angular/core";
+
+@Directive({
+  selector: '[apDarkenOnHover]'
+})
+export class DarkenOnHoverDirective {
+
+  constructor(private el: ElementRef, private render: Renderer2) {
+  }
+  @Input() brightness = '70%';
+
+  @HostListener('mouseover')
+  darkenOn() {
+    this.render.setStyle(this.el.nativeElement, 'filter', `brightness(${this.brightness}`);
+  }
+
+  @HostListener('mouseleave')
+  darkenOff() {
+    this.render.setStyle(this.el.nativeElement, 'filter', 'brightness(100%');
+  }
+}
