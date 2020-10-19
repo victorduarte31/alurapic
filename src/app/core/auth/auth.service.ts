c9ce774d4f4882b2408c875e5a3cb97658f3d7c2
Victor Duarte <victor.mendonca@unigranrio.br>
2020-10-19 10:41:34 -0300

    get user token

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import { tap } from 'rxjs/operators';
 
 const API_URL = 'http://localhost:3000';
 
@@ -12,6 +13,10 @@ export class AuthService {
   }
 
   authenticate(userName: string, password: string) {
-    return this.http.post(API_URL + '/user/login', {userName, password})
+    return this.http.post(API_URL + '/user/login', {userName, password}, {observe: 'response'})
+      .pipe(tap(res => {
+        const authToken = res.headers.get('x-access-token'); // to access the headers it is necessary to pass the note as a parameter in the request call
+        console.log(authToken);
+      }))
   }
 }
