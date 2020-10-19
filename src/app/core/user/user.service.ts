e92ad0602f88b07dc9776df56d69d79963c7463c
Victor Duarte <victor.mendonca@unigranrio.br>
2020-10-19 18:10:31 -0300

    created auth.guard file for route control

--- src/app/core/user/user.service.ts
+++ src/app/core/user/user.service.ts
@@ -1,8 +1,7 @@
 import {Injectable} from '@angular/core';
 import {TokenService} from "../token/token.service";
-import {BehaviorSubject, Subject} from "rxjs";
+import {BehaviorSubject} from "rxjs";
 import {User} from "./user";
-import * as jwt_decode from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +9,7 @@ import * as jwt_decode from 'jwt-decode';
 export class UserService {
 
   private userSubject = new BehaviorSubject<User>(null);
+  private userName: string;
 
   constructor(private tokenService: TokenService) {
     this.tokenService.hasToken() &&
@@ -28,6 +28,7 @@ export class UserService {
   private decodeAndNotify() {
     const token = this.tokenService.getToken();
     const user = this.decode(token) as User
+    this.userName = user.name;
     this.userSubject.next(user);
   }
 
@@ -42,5 +43,13 @@ export class UserService {
   logout() {
     this.tokenService.removeToken();
     this.userSubject.next(null);
+  }
+
+  isLogged() {
+    return this.tokenService.hasToken();
+  }
+
+  getUserName() {
+     return this.userName;
   }
 }
