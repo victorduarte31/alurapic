0c6fe0a933227a30a24501d53fddbc893045d035
Victor Duarte <victor.mendonca@unigranrio.br>
2020-10-20 10:19:19 -0300

    created service to check if user already exists in the application

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {lowerCaseValidator} from "../../shared/Validators/lowerCaseValidator";
+import {UserNotTakenValidatorService} from "./user-not-taken.validator.service";
 
 @Component({
   selector: 'app-signup',
@@ -12,7 +13,8 @@ export class SignupComponent implements OnInit {
   signupForm: FormGroup;
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private userNotTakenValidatorService: UserNotTakenValidatorService
   ) {
   }
 
@@ -20,7 +22,8 @@ export class SignupComponent implements OnInit {
     this.signupForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       userName: ['',
-        [Validators.required, lowerCaseValidator, Validators.minLength(2), Validators.maxLength(30)]
+        [Validators.required, lowerCaseValidator, Validators.minLength(2), Validators.maxLength(30)],
+        this.userNotTakenValidatorService.checkUserNameTaken()
       ],
       fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(40)]],
       password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(14)]],
