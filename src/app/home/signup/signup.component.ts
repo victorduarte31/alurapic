953c1e2546a422c4040a8eebb57f342a300df2fa
Victor Duarte <victor.mendonca@unigranrio.br>
2020-10-20 10:41:50 -0300

    created service to register new user.

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {lowerCaseValidator} from "../../shared/Validators/lowerCaseValidator";
 import {UserNotTakenValidatorService} from "./user-not-taken.validator.service";
+import {NewUser} from "./new-user";
+import {SignupService} from "./signup.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-signup',
@@ -14,7 +17,9 @@ export class SignupComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private userNotTakenValidatorService: UserNotTakenValidatorService
+    private userNotTakenValidatorService: UserNotTakenValidatorService,
+    private signupService: SignupService,
+    private router: Router
   ) {
   }
 
@@ -28,7 +33,13 @@ export class SignupComponent implements OnInit {
       fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(40)]],
       password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(14)]],
     })
-
   }
 
+  signup() {
+    const newUser = this.signupForm.getRawValue() as NewUser;
+    this.signupService.signup(newUser).subscribe(
+      () => this.router.navigate(['']),
+      error => console.log(error)
+    )
+  }
 }
