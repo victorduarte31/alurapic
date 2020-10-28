e6e1791c03c09e59dca0bc551b5d8e87e814eb70
Victor Duarte <victor.mendonca@unigranrio.br>
2020-10-28 08:39:47 -0300

    Added method to calculate percentage of upload

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -2,8 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {PhotoService} from "../photo/photo.service";
 import {Router} from "@angular/router";
-import { ToastrService } from 'ngx-toastr';
+import {ToastrService} from 'ngx-toastr';
 import {UserService} from "../../core/user/user.service";
+import {HttpEvent, HttpEventType, HttpResponse} from "@angular/common/http";
+import {finalize} from "rxjs/operators";
 
 @Component({
   selector: 'ap-photo-form',
@@ -15,6 +17,7 @@ export class PhotoFormComponent implements OnInit {
   photoForm: FormGroup;
   file: File;
   preview: string;
+  percentDone: number = 0;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -33,20 +36,28 @@ export class PhotoFormComponent implements OnInit {
   }
 
   upload() {
-    const description = this.photoForm.get('description').value;
-    const allowComments = this.photoForm.get('allowComments').value;
+    if (this.file) {
+      const description = this.photoForm.get('description').value;
+      const allowComments = this.photoForm.get('allowComments').value;
 
-    this.photoService.upload(description, allowComments, this.file)
-        .subscribe(() => {
-          this.toastr.success('Upload complete');
+      this.photoService.upload(description, allowComments, this.file)
+        .pipe(finalize(() => {
           this.router.navigate(['/user', this.userService.getUserName()])
-        })
+        }))
+        .subscribe((event: HttpEvent<any>) => {
+          if (event.type == HttpEventType.UploadProgress) {
+            this.percentDone = Math.round(100 * event.loaded / event.total);
+          } else if (event instanceof HttpResponse) {
+            this.toastr.success('Upload complete');
+          }
+        }, error => this.toastr.error('Upload error!', 'ERROR'));
+    }
   }
 
   handleFile(file: File) {
     this.file = file;
     const reader = new FileReader();
-    reader.onload = (event : any) => this.preview = event.target.result;
+    reader.onload = (event: any) => this.preview = event.target.result;
     reader.readAsDataURL(file);
   }
 }
